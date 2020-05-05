20d476d0a90e7ebb585065f6f57f6e27281609d3
Henrique Barbosa de Souza Jr <henrique@hbveterinaria.com.br>
2020-05-05 01:14:12 -0300

    Criação do Servidor e conexão com o Banco de Dados MySQL / knex

---
+++ knexfile.js
@@ -0,0 +1,19 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      user : 'root',
+      password : '12345678',
+      database : 'banco'
+    },
+
+    migrations:{
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/src/database/migrations`
+    }
+  },
+
+};
